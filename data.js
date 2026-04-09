206553fd99e0864cdfbc5e55d996bf7467c34783
ght-111 <1187350674@qq.com>
2026-04-10 07:14:43 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+window.certData = {
+  "config": {
+    "orderNumber": true,
+    "instrumentName": false,
+    "manufacturer": false,
+    "factoryNumber": false,
+    "model": false
+  },
+  "certs": {
+    "11222": {
+      "orderNumber": "11122222222222222222222222",
+      "client": "222",
+      "instrumentName": "222",
+      "manufacturer": "222",
+      "calibrationDate": "2026-04-09",
+      "expiryDate": "2026-04-03",
+      "factoryNumber": "222",
+      "model": "222"
+    }
+  }
+};
